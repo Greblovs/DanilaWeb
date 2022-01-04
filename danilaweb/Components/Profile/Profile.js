decd21a726e0c044acd68c37eefde4348774d1df
vega <S.greblov@gmail.com>
2022-01-03 22:02:37 -0800

    first profile

---
+++ danilaweb/Components/Profile/Profile.js
@@ -0,0 +1,9 @@
+import React, {Component, useCallback, useEffect, useRef, useState} from "react";
+import classes from "./Profile.module.scss"
+
+
+const Profile = () =>{
+    <p>Danila Lox</p>
+}
+
+export default Profile;
